9883e1e3477ce74b24e3169be9c16ea830f2b1b1
Albert Lay <albertjlay@gmail.com>
2021-09-18 14:33:03 -0400

    Make CovidStats dynamic

---
+++ frontend/src/utils.js
@@ -0,0 +1,11 @@
+export function regionActiveCasesPerMillionStyle(regionData) {
+  return regionData.active_cases_per_million <= 700 ? 'safe' : 'warning';
+}
+
+export function regionFullyVaccinatedProportionStyle(regionData) {
+  return regionData.fully_vaccinated_proportion > 0.75 ? 'safe' : 'warning';
+}
+
+export function regionVulnerableProportionStyle(regionData) {
+  return regionData.vulnerable_proportion <= 0.03 ? 'safe' : 'warning';
+}
