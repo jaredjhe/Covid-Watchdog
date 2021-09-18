ac9eaa6720e8b54ab8fa300b8919a17c249fef77
Albert Lay <albertjlay@gmail.com>
2021-09-18 16:23:16 -0400

    Connect FullRegions to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
 import FullRegions from './components/FullRegions';
-import React, { Component } from 'react';
+import React from 'react';
 
 import TestMap from './components/MapTest';
 
 import './styles/App.scss';
 import './styles/CovidStats.scss'
 import './styles/FullRegions.scss'
+import CovidStats from './components/CovidStats';
 
 function App() {
   return (
     <div>
       <TestMap />
+      <FullRegions />
     </div>
   );
 }
--- frontend/src/components/FullRegions.jsx
+++ frontend/src/components/FullRegions.jsx
@@ -1,36 +1,46 @@
+import { useState, useEffect } from 'react';
 import travelIcon from '../assets/travel.svg'
 import homeIcon from '../assets/relaxing-at-home.svg'
 import CovidStats from './CovidStats';
 
-function FullSafeRegions() {
-  return (
-    <section className="safe-regions">
-      <div className="call-to-action">
-        <img src={travelIcon} alt="Two person traveling."/>
-        <h2>Let's book that trip!</h2>
-      </div>
-      <CovidStats displayHeader />
-    </section>
-  )
-}
 
-function FullWarningRegions() {
-  return (
-    <section className="warning-regions">
-      <div className="call-to-action">
-        <h2>Maybe Netflix would be better?</h2>
-        <img src={homeIcon} alt="A person chilling at home."/>
-      </div>
-      <CovidStats displayHeader />
-    </section>
-  )
-}
 
-function FullRegions() {
+/**
+ * Component which will show up after the user click on a province in the map.
+ */
+function FullRegions(props) {
+  const { provinceCode } = props; 
+  const [regionData, setRegionData] = useState([]);
+
+  useEffect(() => {
+    fetch(`/api/v1/CanadaCovidInfo/${provinceCode}/provinceInfo`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setRegionData(result);
+        },
+        (error) => {
+          throw new Error(`Cannot reach server. Try again later! ${error}`)
+        }
+      )
+  }, [provinceCode])
+
   return (
     <section className="full-regions">
-      <FullSafeRegions />
-      <FullWarningRegions />
+      <div className="safe-regions">
+        <div className="call-to-action">
+          <img src={travelIcon} alt="Two person traveling."/>
+          <h2>Let's book that trip!</h2>
+        </div>
+        <CovidStats displayHeader regionsData={regionData.filter(data => data.is_safe)}/>
+      </div>
+      <div className="warning-regions">
+        <div className="call-to-action">
+          <h2>Maybe Netflix would be better?</h2>
+          <img src={homeIcon} alt="A person chilling at home."/>
+        </div>
+        <CovidStats displayHeader regionsData={regionData.filter(data => !data.is_safe)}/>
+      </div>
     </section>
   )
 }
