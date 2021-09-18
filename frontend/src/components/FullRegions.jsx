10d8c99325f64813706e8ace3868a3e35314bf3c
Dylex Suan <dylexms@gmail.com>
2021-09-18 18:58:09 -0400

    javascript async temp fix

--- frontend/src/components/FullRegions.jsx
+++ frontend/src/components/FullRegions.jsx
@@ -71,13 +71,21 @@ function FullRegions(props) {
       try {
         const provinceDataJson = await fetch(`http://localhost:8000/api/v1/CanadaCovidInfo/${provinceCode}/provinceInfo`);
         const provinceData = await provinceDataJson.json();
-        console.log(provinceData)
-        const regionsDataArray = Promise.all((provinceData.regions).map(async (el) => await fetch(`http://localhost:8000/api/v1/CanadaCovidInfo/${provinceCode}/${el.hr_uid}/regionInfo`)))
+        // console.log(provinceData)
+        const regionsDataArray = await Promise.all((provinceData.regions).map(async (el) => await fetch(`http://localhost:8000/api/v1/CanadaCovidInfo/${provinceCode}/${el.hr_uid}/regionInfo`)))
           .then(res => {
+            let resArray = [];
             for (let i = 0; i < res.length; ++i) {
-              
+              resArray = resArray.concat(res[i].json());
             }
-          });
+            return resArray;
+          }).then(data => {
+            let arrayOfPromises = data;
+            console.log(arrayOfPromises);
+          })
+
+
+        // const jsonregionsDataArray = regionsD
         // .then(res => {
         //   Promise.all(regionsDataJsonArray.map(async (el) => await el.json()))
         // });
@@ -95,8 +103,7 @@ function FullRegions(props) {
         //   return curRegionData;
         // return await fetch(`/api/v1/CanadaCovidInfo/${provinceCode}/${el.hr_uid}/regionInfo`);
         // })
-        console.log("Hello")
-        console.log(regionsDataArray);
+
         // setRegionData(regionsDataJsonArray);
       }
       catch (error) {
