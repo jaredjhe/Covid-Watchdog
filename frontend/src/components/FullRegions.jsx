f38b1ad27c403e4f82aa23011c81fb34c16dfbea
Albert Lay <albertjlay@gmail.com>
2021-09-18 18:22:11 -0400

    Broken FullRegions for Dylex to review

--- frontend/src/components/FullRegions.jsx
+++ frontend/src/components/FullRegions.jsx
@@ -67,17 +67,37 @@ function FullRegions(props) {
   const [regionData, setRegionData] = useState([]);
 
   useEffect(() => {
-    console.log(provinceCode);
-    fetch(`http://localhost:8000/api/v1/CanadaCovidInfo/${provinceCode}/provinceInfo`)
-      .then(res => res.json())
-      .then(
-        result => {
-          console.log(result)
-          setRegionData(result);
-        }
-      ).catch((error) => {
-        throw new Error(`Cannot reach server. Try again later! ${error}`)
-      })
+    async function fetchData() {
+      try {
+        const provinceDataJson = await fetch(`http://localhost:8000/api/v1/CanadaCovidInfo/${provinceCode}/provinceInfo`);
+        const provinceData = await provinceDataJson.json();
+        console.log(provinceData)
+        const regionsDataArray = Promise.all(provinceData.regions.map(async (el) => await fetch(`/api/v1/CanadaCovidInfo/${provinceCode}/${el.hr_uid}/regionInfo`)))
+        .then(res => {
+          Promise.all(regionsDataJsonArray.map(async (el) => await el.json()))
+        });
+        const regionsDataArray = await ;
+        // const x = await regionsDataJsonArray[0].json();
+       
+        // console.log(x);
+        
+
+
+        // const regionsDataArray = provinceData.regions.map(async (el) => {
+        //   const curRegionDataJson = await fetch(`/api/v1/CanadaCovidInfo/${provinceCode}/${el.hr_uid}/regionInfo`);
+        //   const curRegionData = await curRegionDataJson.json();
+        //   console.log(curRegionData);
+        //   return curRegionData;
+          // return await fetch(`/api/v1/CanadaCovidInfo/${provinceCode}/${el.hr_uid}/regionInfo`);
+        // })
+        // console.log(regionsDataArray);
+        setRegionData(regionsDataJsonArray);
+      }
+      catch(error) {
+        throw new Error(error)
+      }
+    }
+    fetchData();
   }, [provinceCode])
 
   return (
