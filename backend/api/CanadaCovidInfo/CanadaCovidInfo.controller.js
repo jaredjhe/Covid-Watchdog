1c27bab2eefa0f5416238140295cd442055e8ed1
mhahelwa2020 <mhahelwa@gmail.com>
2021-09-18 16:01:44 -0400

    Modified API

--- backend/api/CanadaCovidInfo/CanadaCovidInfo.controller.js
+++ backend/api/CanadaCovidInfo/CanadaCovidInfo.controller.js
@@ -1,5 +1,47 @@
 import request from "request"
 
+const population = {
+    ON: 14789778,
+    QC: 8585523,
+    BC: 5174724,
+    MB: 1382904,
+    NT: 44991,
+    NS: 982326,
+    NU: 39536,
+    PE: 160536,
+    SK: 1179906,
+    YT: 42596,
+    NB: 783721,
+    AB: 4444277,
+    NL: 520286
+}
+
+const baselineActiveCases = 700
+const baselineFullyVacxxed = 0.75
+const baselineVulnerable = 0.25
+const vaccinatedRiskConstant = 0.0008
+
+function getDateFormat (date) {
+    var dd = date.getDate();
+
+    var mm = date.getMonth()+1; 
+    var yyyy = date.getFullYear();
+    
+    if(dd<10) 
+    {
+        dd='0'+dd;
+    } 
+
+    if(mm<10) 
+    {
+        mm='0'+mm;
+    } 
+
+    return (dd+'-'+mm+'-'+yyyy);
+
+}
+
+
 /**
  * Canada Covid Info class.
  */
@@ -12,24 +54,68 @@ export default class CanadaCovidInfo {
      * @param {*} next 
      */
     static async asyncGetCovidInfoProvince(req, res, next) {
-        let prov = req.params.prov
-        request('https://api.covid19tracker.ca/summary/split', function (error, response, body) {
+        let province = (req.params.prov)?.toUpperCase()
+        
+
+        request(`https://api.opencovid.ca/summary?loc=${province}`, function (error, response, body) {
             if (!error && response.statusCode == 200) {
-                let data = JSON.parse(body)["data"]
-                for (let elem in data) {
-                    if (data[elem]["province"] === prov) {
-                        let finalResponse = {
-                            prov: data[elem]["province"],
-                            date: data[elem]["date"],
-                            new_cases: data[elem]["change_cases"],
-                            new_death: data[elem]["change_fatalities"],
-                            total_cases: data[elem]["total_cases"],
-                            total_death: data[elem]["total_fatalities"],
-                            fully_vaccinated: data[elem]["total_vaccinated"]
-                        }
-                        res.json(finalResponse)
-                        return
+                let data = JSON.parse(body)["summary"]
+                for (let elem in data) {       
+
+                    let active_cases_per_million = 1000000 * (data[elem]["cumulative_cases"] - data[elem]["cumulative_deaths"] - data[elem]["cumulative_recovered"]) / population[province]
+                    let active_cases_per_million_safe =  active_cases_per_million <= baselineActiveCases
+
+                    let fully_vaccinated_proportion = data[elem]["cumulative_cvaccine"] / population[province]
+                    let fully_vaccinated_proportion_safe = fully_vaccinated_proportion >= baselineFullyVacxxed
+
+                    let vulnerable_proportion = ((population[province] - data[elem]["cumulative_cvaccine"])/ population[province]) + vaccinatedRiskConstant
+                    let vulnerable_proportion_safe = vulnerable_proportion <= baselineVulnerable
+
+
+                    let finalResponse = {
+                        prov: province,
+                        date: data[elem]["date"],
+                        new_cases: data[elem]["cumulative_cases"] - data[elem]["cumulative_deaths"] - data[elem]["cumulative_recovered"],
+                        new_death: data[elem]["change_fatalities"],
+                        active_cases: data[elem]["active_cases"],
+                        total_cases: data[elem]["cumulative_cases"],
+                        total_death: data[elem]["cumulative_deaths"],
+                        fully_vaccinated: data[elem]["cumulative_cvaccine"],
+                        active_cases_per_million: active_cases_per_million,
+                        active_cases_per_million_safe: active_cases_per_million_safe,
+                        fully_vaccinated_proportion: fully_vaccinated_proportion,
+                        fully_vaccinated_proportion_safe: fully_vaccinated_proportion_safe,
+                        vulnerable_proportion: vulnerable_proportion,
+                        vulnerable_proportion_safe: vulnerable_proportion_safe,
+                        regions: []
                     }
+
+                    return new request('https://api.opencovid.ca/other?stat=hr', function (regionError, regionResponse, regionBody) {
+                        if (!regionError && regionResponse.statusCode == 200) {
+                            let region = JSON.parse(regionBody)["hr"]
+            
+                            if (region !== undefined) {
+                                let regionList = []
+                                for (var elem in region) {
+                                    if (region[elem]["province_short"] == province) {
+                                        let regionInfo = {
+                                            hr_uid: region[elem]["HR_UID"],
+                                            health_region: region[elem]["health_region"],
+                                            health_region_health_org:  region[elem]["health_region_esri"],
+                                            prov:  region[elem]["province_short"]
+                                        }
+                                        regionList.push(regionInfo)  
+                                    }
+                                }
+                                finalResponse.regions = regionList
+                            }
+                            res.json(finalResponse)
+                            return
+                        } else {
+                            res.json(finalResponse)
+                            return
+                        }
+                    });            
                 }
                 res.status(400).json({ error: "Province not found" })
                 return
@@ -40,54 +126,102 @@ export default class CanadaCovidInfo {
         });
     }
 
+
     static async asyncGetCovidInfoRegion(req, res, next) {
         let province = req.params.prov
-        let region = req.params.reg.toUpperCase() 
+        let regionId = parseInt(req.params.regId) 
+        console.log(regionId)
+        var today = new Date();
+        var date = new Date(today)
+
+        date.setDate(date.getDate() - 1)
         
-        request('https://api.covid19tracker.ca/regions', function (regionError, regionResponse, regionBody) {
-            if (!regionError && regionResponse.statusCode == 200) {
-                let regionData = JSON.parse(regionBody)["data"]
+        let dateStr = getDateFormat(date)
+        console.log(dateStr)
+        request('https://api.covid19tracker.ca/summary/split/hr', function (hrError, hrResponse, hrBody) {
+            if (!hrError && hrResponse.statusCode == 200) {
+                let data = JSON.parse(hrBody)["data"]
 
-                for (let elem in regionData) {
-                    let regionHealthService = (regionData[elem]["engname"]).toString().toUpperCase()
+                for (let elem in data) {
+                    if (data[elem]["hr_uid"] == regionId) {
+                        let finalResponse = {
+                            prov: province,
+                            region: null,
+                            date: data[elem]["date"],
+                            new_cases: data[elem]["change_cases"],
+                            new_death: data[elem]["change_fatalities"],
+                            total_cases: data[elem]["total_cases"],
+                            total_death: data[elem]["total_fatalities"],
+                            fully_vaccinated: data[elem]["total_vaccinated"],
+                            active_cases: "",
+                            population: "",
+                            covid_watchdog_scale:"",
+                            active_cases_per_million: null,
+                            active_cases_per_million_safe: null,
+                            fully_vaccinated_proportion: null,
+                            fully_vaccinated_proportion_safe: null,
+                            vulnerable_proportion: null,
+                            vulnerable_proportion_safe: null,
+                        }
 
-                    if (regionData[elem]["province"] === province && regionHealthService.includes(region) !== -1) {
-                        let regionId = regionData[elem]["hr_uid"]
+                        return new  request('https://api.opencovid.ca/other?stat=hr', function (popError, popResponse, popBody) {
+                            if (!popError && popResponse.statusCode == 200) {
+                                let regionList = JSON.parse(popBody)["hr"]
 
-                        return new request('https://api.covid19tracker.ca/summary/split/hr', function (error, response, body) {
-                            if (!error && response.statusCode == 200) {
-                                let data = JSON.parse(body)["data"]
-                                for (let obj in data) {
-                                    if (data[obj]["hr_uid"] === regionId) {
-                                        let finalResponse = {
-                                            prov: province,
-                                            reg: region,
-                                            date: data[elem]["date"],
-                                            new_cases: data[elem]["change_cases"],
-                                            new_death: data[elem]["change_fatalities"],
-                                            total_cases: data[elem]["total_cases"],
-                                            total_death: data[elem]["total_fatalities"],
-                                            fully_vaccinated: data[elem]["total_vaccinated"]
-                                        }
+                                for (var i in regionList) {
+                                    if (regionList[i]["HR_UID"] == regionId) {
+                                        let regionPop = regionList[i]["pop"]
+                                        finalResponse.population = regionPop
+                                        finalResponse.region = regionList[i]["health_region"]
 
-                                        
-                                        res.json(finalResponse)
-                                        return
-                                    } 
+                                        return  new request(`https://api.opencovid.ca/timeseries?stat=cases&loc=${regionId}&date=${dateStr}`, function (casesError, casesResponse, casesBody){
+                                            if (!casesError && casesResponse.statusCode == 200) {
+                                                let caseData = JSON.parse(casesBody)["cases"][0]
+                                                console.log(caseData)
+
+                                                if (caseData.length !== 0) {
+                                                    let active_cases_per_million = 1000000 * (caseData["cases"]) / regionPop
+                                                    let active_cases_per_million_safe =  active_cases_per_million <= baselineActiveCases
+                                
+                                                    let fully_vaccinated_proportion = data[elem]["total_vaccinated"] / regionPop
+                                                    let fully_vaccinated_proportion_safe = fully_vaccinated_proportion >= baselineFullyVacxxed
+                                
+                                                    let vulnerable_proportion = ((regionPop - data[elem]["total_vaccinated"])/ regionPop) + vaccinatedRiskConstant
+                                                    let vulnerable_proportion_safe = vulnerable_proportion <= baselineVulnerable
+
+                                                    finalResponse.active_cases_per_million = active_cases_per_million
+                                                    finalResponse.active_cases_per_million_safe = active_cases_per_million_safe
+                                                    finalResponse.fully_vaccinated_proportion = fully_vaccinated_proportion
+                                                    finalResponse.fully_vaccinated_proportion_safe = fully_vaccinated_proportion_safe
+                                                    finalResponse.vulnerable_proportion = vulnerable_proportion
+                                                    finalResponse.vulnerable_proportion_safe = vulnerable_proportion_safe
+
+                                                    res.json(finalResponse)
+                                                    return
+
+                                                }
+                                            } else {
+                                                res.status(400).json({ error: "Province or region not found" })
+                                                return
+                                            }
+                                        });
+                                    }
                                 }
-                                res.status(400).json({ error: "Province or region not found" })
+                            } else {
+                                res.status(404).json({ error: "Not found" })
                                 return
                             }
                         });
-                        break
-                    }                   
+                        res.json(finalResponse)
+                        return
+                    } 
                 }
                 res.status(400).json({ error: "Province or region not found" })
                 return
-            } else {
-                res.status(404).json({ error: "Not found" })
-                return
             }
         });
+
     }
+
+
 }
--- backend/api/CanadaCovidInfo/CanadaCovidInfo.route.js
+++ backend/api/CanadaCovidInfo/CanadaCovidInfo.route.js
@@ -4,7 +4,7 @@ import CanadaCovidInfo from "./CanadaCovidInfo.controller.js"
 const router = express.Router()
 
 router.route("/:prov/proviceInfo").get(CanadaCovidInfo.asyncGetCovidInfoProvince)
-router.route("/:prov/:reg/regionInfo").get(CanadaCovidInfo.asyncGetCovidInfoRegion)
+router.route("/:prov/:regId/regionInfo").get(CanadaCovidInfo.asyncGetCovidInfoRegion)
 
 
 export default router
